55981b7de09ae3b221647c816b9bd7b9844460f4
sciencewithsaurav <53606230+sciencewithsaurav@users.noreply.github.com>
2019-09-25 22:48:38 +0530

    Add files via upload

--- Pollution_Hackathon.R
+++ Pollution_Hackathon.R
@@ -16,29 +16,23 @@ avg_SO2 <- mean(dataset$SO2)
 avg_NO2 <- mean(dataset$NO2)
 avg_PM10 <- mean(dataset$PM10)
 
-#How does SO2, NO2 and PM10 change over time for each state?
-p <- dataset %>% group_by(State) %>%
-  ggplot(aes(x = date, y = PM10)) +
-  geom_smooth(method = lm, se = FALSE,col="red") +
-  scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
-  geom_hline(yintercept = mean(dataset$PM10), col="green") +
-  geom_text(aes(mean(date),mean_PM10,label = "Mean PM10", vjust = 0.5))+
-  theme_classic()
-#p + geom_line()
-
-#Display state wise below and abobe concentration (diverging graph)
 
 #Which state saw the maximum increase in SO2, NO2 and PM10?
-dataset %>% filter(SO2 > 0) %>% 
-  group_by(State) %>% summarize(avg_SO2 = mean(SO2)) %>%
+dataset %>% filter(SO2 > 0 & year %in% c("2011","2015")) %>% 
+  group_by(State,year) %>% summarize(avg_SO2 = mean(SO2)) %>%
   ggplot(aes(x=reorder(State,avg_SO2),y=avg_SO2, fill=avg_SO2>mean(dataset$SO2))) + 
-  geom_bar(stat='identity') + 
+  geom_bar(stat='identity', position="dodge") + 
+  geom_text(aes(label=floor(avg_SO2)),position=position_stack()) +
   #geom_text(aes(label=avg_SO2), vjust=0) 
   geom_hline(yintercept = mean(dataset$SO2), col="red") +
   coord_flip() +
   theme(legend.position = "none") +   #removing legend
   ylab("Average SO2") +
-  xlab("States")
+  xlab("States") +
+  facet_wrap(~year)
+
+
+#Display state wise below and abobe concentration (diverging graph)
 
 #Which year saw the maximum increase in SO2, NO2 and PM10?
 
